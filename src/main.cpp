cc3206a1a4214b35d76a0aba602446625024c2a1
Martin Gspurning <gspurningm@gmail.com>
2026-04-06 13:34:14 +0200

    feat(api): connect VariableEngine to GET endpoint with error handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,31 @@
+#include "axMini/Variable.hpp"
+#include "axMini/VariableEngine.hpp"
 #include "httplib.h"
-#include <iostream>
+#include <string>
 
 int main(int argc, char *argv[]) {
-
+  VariableEngine engine;
   httplib::Server svr;
 
-  svr.Get("/variables/test", [](const httplib::Request &,
-                                httplib::Response &res) {
-    res.set_content("{\"name\": \"test\", \"value\": 42}", "application/json");
-  });
+  Variable input{VariableType::Input, "input_test", 42};
+  Variable output{VariableType::Output, "output_test", true};
+
+  engine.AddVariable(input);
+  engine.AddVariable(output);
+
+  svr.Get("/variables/test",
+          [&engine](const httplib::Request &, httplib::Response &res) {
+            auto it = engine.GetVariable("input_test");
+            std::string json;
+            if (it.has_value()) {
+              json += "{\"name\": \"" + it->name + "\", \"value\": 42}";
+              res.set_content(json, "application/json");
+            } else {
+              res.status = 404;
+              res.set_content("{\"error\": \"variable not found\"}",
+                              "application/json");
+            }
+          });
 
   svr.listen("0.0.0.0", 8080);
   return 0;
